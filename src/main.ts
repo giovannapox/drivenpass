536e8f002cb70361342c021097b802542d249330
Giovanna <giovannad1clonius@gmail.com>
2023-09-03 19:07:36 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-const port = process.env.PORT || 3000
 async function bootstrap() {
+  const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
