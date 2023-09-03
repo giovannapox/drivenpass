0518a2da0db7ba5f93fa356cbc7284a0b47a9db3
Giovanna <giovannad1clonius@gmail.com>
2023-09-03 19:22:51 -0300

    fix controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,6 @@
-import { Controller, Body, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Body, Get, HttpCode, HttpStatus, Post, Request} from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from 'src/guards/auth.guard';
 
-@UseGuards(AuthGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
